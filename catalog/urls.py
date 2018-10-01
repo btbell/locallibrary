3031140a051930330a8acb4b1896a34569186625
btbell <btbell@gmail.com>
2018-10-01 14:43:47 -0400

    test

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('hello', views.hello, name='hello'),
+  path('hello/', views.hello, name='hello'),
   path('', views.index, name='index'),
   path('catalog/foo', views.foo, name='foo')
 ]
